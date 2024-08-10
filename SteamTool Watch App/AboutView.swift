929270c07ae6fc33f2de26080df6c6d0567b7652
Maverick Charmer <sjbstudio233@gmail.com>
2024-08-10 09:22:27 +0800

    修改头像 拯救坠机的Git 增加备案号 更新Cepheus 将CachedWebImage更换为SDWebImageSwiftUI

---
+++ SteamTool Watch App/AboutView.swift
@@ -0,0 +1,183 @@
+//
+//  AboutView.swift
+//  SteamTool Watch App
+//
+//  Created by 周敬博 on 2023/9/15.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        NavigationView {
+            Form {
+                Section(header: Text("关于我们")) {
+                    NavigationLink(destination: About()){
+                        HStack {
+                            Image(systemName: "info.circle")
+                                .foregroundColor(.blue)
+                            Text(" 应用信息")
+                        }
+                    }
+                    NavigationLink(destination: TeamView()){
+                        HStack {
+                            Image(systemName: "person.2")
+                                .foregroundColor(.blue)
+                            Text("开发团队")
+                        }
+                    }
+                    NavigationLink(destination: Xuke()){
+                        HStack {
+                            Image(systemName: "shippingbox")
+                                .foregroundColor(.blue)
+                            Text(" 开源组件许可")
+                        }
+                    }
+                    NavigationLink(destination: SettingView()){
+                        HStack {
+                            Image(systemName: "gear")
+                                .foregroundColor(.blue)
+                            Text(" 设置")
+                        }
+                    }
+                }
+                Section(header: Text("联系我们")) {
+                    HStack {
+                        Image(systemName: "envelope")
+                            .foregroundColor(.blue)
+                        Link("sjbstudio233@gmail.com", destination: URL(string: "mailto:$sjbstudio233@gmail.com")!)
+                    }
+                }
+                Section(header: Text("备案号")) {
+                    Text("浙ICP备2024071295号-16A")
+                }
+
+            }
+        }
+    }
+}
+
+#Preview {
+    AboutView()
+}
+
+struct TeamView: View{
+    let url = Bundle.main.url(forResource: "qwasd头像", withExtension: "gif")
+    var body: some View{
+        Form{
+            HStack {
+                WebImage(url: url)
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .clipShape(Circle())
+                VStack(alignment: .leading) {
+                    Text("qwasd")
+                        .font(.title3)
+                    Text("开发者")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+            HStack {
+                Image("幼雾酱头像")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .clipShape(Circle())
+                VStack(alignment: .leading) {
+                    Text("幼雾酱")
+                        .font(.title3)
+                    Text("图标设计")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+            HStack {
+                Image("785头像")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .clipShape(Circle())
+                VStack(alignment: .leading) {
+                    Text("ThreeManager785")
+                        .font(.title3)
+                    Text("英文翻译")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+            NavigationLink(destination: TebiemingxieView()){
+                HStack {
+                    Image(systemName: "fireworks")
+                        .foregroundColor(.red)
+                    Text("特别鸣谢")
+                }
+            }
+        }
+    }
+}
+
+struct About:View {
+    var body: some View {
+        List{
+            Section{
+                Text("开发者是一名高中生，因为在学校里常年不知道Steam促销的信息而经常错过打折，于是写了这款软件，方便在学校里查看Steam的促销信息")
+                Text("希望它也能帮助您不落下每一次促销")
+                Text("使用愉快🌹🌹")
+            }
+        }
+    }
+}
+
+struct Xuke:View {
+    let openSourceTexts = """
+                --- SwiftSoup ---
+                Licensed under MIT license
+                -----------------
+                
+                 --- SFSymbol ---
+                Licensed under MIT license
+                -----------------
+                
+                 - SDWebImageSwiftUI -
+                Licensed under MIT license
+                -----------------------
+                
+                --- Cepheus ---
+                Licensed under Apache-2.0 license
+                -----------------
+                
+                """
+    var body: some View {
+        ScrollView{
+            Text(openSourceTexts)
+        }
+    }
+}
+
+struct TebiemingxieView:View {
+    var body: some View {
+        Form{
+            HStack {
+                Image("MEMZ头像")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .clipShape(Circle())
+                VStack(alignment: .leading) {
+                    Text("WindowsMEMZ")
+                    Text("大师救救😭")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+        }
+    }
+}
+
+struct SettingView:View {
+    @AppStorage("useCepheus") var useCepheus:Bool = true
+    var body: some View {
+        Toggle(isOn: $useCepheus){
+            Text("使用Cephues键盘")
+        }
+    }
+}
---
+++ SteamTool Watch App/ContentView.swift
@@ -0,0 +1,275 @@
+//
+//  ContentView.swift
+//  SteamTool Watch App
+//
+//  Created by Maverick Charmer on 2023/9/15.
+//
+
+import SwiftUI
+import SwiftSoup
+import SDWebImageSwiftUI
+import Cepheus
+
+struct Game: Identifiable {
+    let id = UUID()
+    let title: String
+    let originalPrice: String
+    let discountPrice: String
+    let discountPercent: String
+    let picurl: String
+}
+
+struct GameView: View {
+    let game: Game
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(game.title)
+                    .font(.headline)
+                Text(game.originalPrice)
+                    .strikethrough()
+                    .foregroundColor(.gray)
+                Text(game.discountPrice)
+                    .foregroundColor(.red)
+            }
+            Spacer()
+            if game.discountPercent != ""{
+                Text(game.discountPercent)
+                    .padding(5)
+                    .background(Color.green)
+                    .cornerRadius(5)
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View{
+        TabView{
+            MainView()
+                .tag(1)
+            AboutView()
+                .tag(2)
+        }
+    }
+}
+
+struct searchView:View {
+    @State var content:String = ""
+    @State private var badNetwork = false
+    @State var games:[Game] = []
+    @State var isNotSearched = true
+    @State var num = 1
+    @AppStorage("useCepheus") var useCepheus:Bool = true
+    
+    var body: some View {
+        ZStack{
+            if isNotSearched {
+                CepheusKeyboard(input: $content,
+                                prompt: "请输入游戏名",
+                                CepheusIsEnabled:useCepheus,allowEmojis: false
+                    ,onSubmit: {
+                        searchGame(content: content)
+                        isNotSearched.toggle()
+                  })
+            }else{
+                    NavigationView {
+                        ZStack{
+                            List(games) { game in
+                                NavigationLink(destination: detailView(game: game)){
+                                    GameView(game: game)}
+                            }
+                            if games.isEmpty && !badNetwork{
+                                ProgressView()
+                            }
+                            if badNetwork{
+                                Button{
+                                    num += 1
+                                }label: {
+                                    Image(systemName:"network.slash")
+                                        .symbolEffect(.bounce.up.byLayer,value:num)
+                                        .font(.largeTitle)
+                                }.buttonStyle(PlainButtonStyle())
+                            }
+                        }
+                        }
+                        .onAppear {
+                            searchGame(content: content )
+                        }
+                    }
+                }
+        }
+    func searchGame(content:String){
+        let baseurl = "https://store.steampowered.com/search/?term="
+        let content = content
+        
+        let url = baseurl + content
+        
+        let task = URLSession.shared.dataTask(with: URL(string: url)!) { data, response, error in
+            if let error = error {
+                badNetwork = true
+                print("网络请求失败：\(error.localizedDescription)")
+                return
+            }
+            guard let data = data else {
+                print("没有获取到数据")
+                return
+            }
+            do {
+                let html = String(data: data, encoding: .utf8)
+                let doc = try SwiftSoup.parse(html ?? "")
+                // 提取游戏列表的元素
+                let games = try doc.select("div#search_resultsRows > a")
+                // 创建一个空数组来存储游戏对象
+                var gamesArray:[Game] = []
+                // 遍历每个游戏元素，并提取相关信息
+                for game in games {
+                    // 提取游戏标题
+                    let title = try game.select("span.title").text()
+                    // 提取游戏原价
+                    let originalPrice = try game.select("div.discount_original_price").text()
+                    // 提取游戏折扣价
+                    let discountPrice = try game.select("div.discount_final_price").text()
+                    // 提取游戏折扣比例
+                    let discountPercent = try game.select("div.discount_pct").text()
+                    let imgTag: Element = try! game.select("img").first()!
+                    // 提取游戏图片
+                    let pic = try imgTag.attr("src")
+                    let game = Game(title: title, originalPrice: originalPrice, discountPrice: discountPrice , discountPercent: discountPercent,picurl: pic)
+                    gamesArray.append(game)
+                }
+                DispatchQueue.main.async {
+                    self.games = gamesArray
+                }
+            } catch {
+                print("解析HTML失败：\(error.localizedDescription)")
+            }
+        }
+        task.resume()
+    }
+}
+
+struct detailView:View {
+    let game: Game
+    private static let standartPadding: CGFloat = 20
+    
+    var body: some View {
+        GeometryReader { geometry in
+            let size = geometry.size
+            VStack{
+                WebImage(url:URL(string:game.picurl))
+                .frame(
+                    maxWidth: size.width - Self.standartPadding * 2,
+                    idealHeight:
+                        getIdealHeight(
+                            geometrySize: size,
+                            aspectRatio: 2 / 3
+                        )
+                )
+                GameView(game: game)
+            }
+        }
+    }
+    private func getIdealHeight(
+        geometrySize: CGSize,
+        aspectRatio: CGFloat
+    ) -> CGFloat {
+        let width = geometrySize.width - Self.standartPadding * 2
+        return width / aspectRatio
+    }
+}
+
+struct MainView: View {
+    @State var games:[Game] = []
+    @State var isGoToAbout = false
+    @State var badNetwork = false
+    
+    var body: some View {
+            ZStack{
+                NavigationStack {
+                    List(games) { game in
+                        NavigationLink(destination: detailView(game: game)){
+                            GameView(game: game)}
+                    }
+                    .navigationTitle("Steam特惠游戏")
+                    .navigationBarTitleDisplayMode(.large)
+                    .toolbar {
+                        ToolbarItem(placement: .topBarLeading) {
+                            Button(action: {
+                                isGoToAbout = true
+                            }, label: {
+                                Image(systemName: "magnifyingglass")
+                                    .foregroundColor(.gray)
+                            })
+                            .sheet(isPresented: $isGoToAbout, content: {searchView()})
+                        }
+                    }
+                    .onAppear {
+                        if games.isEmpty{
+                            getGames()
+                        }
+                        print("Init")
+                    }
+                }.alert(isPresented: $badNetwork, content: {
+                    Alert(title: Text("网络请求失败"),dismissButton: .cancel())
+                })
+                if games.isEmpty {
+                    ProgressView()
+                }
+            }
+        }
+    
+    func getGames(){
+        let url = "https://store.steampowered.com/search/?filter=topsellers&specials=1"
+        let task = URLSession.shared.dataTask(with: URL(string: url)!) { data, response, error in
+            if let error = error {
+                badNetwork = true
+                print("网络请求失败：\(error.localizedDescription)")
+                return
+            }
+            guard let data = data else {
+                print("没有获取到数据")
+                return
+            }
+            do {
+                let html = String(data: data, encoding: .utf8)
+                let doc = try SwiftSoup.parse(html ?? "")
+                // 提取游戏列表的元素
+                let games = try doc.select("div#search_resultsRows > a")
+                // 创建一个空数组来存储游戏对象
+                var gamesArray:[Game] = []
+                // 遍历每个游戏元素，并提取相关信息
+                for game in games {
+                    // 提取游戏标题
+                    let title = try game.select("span.title").text()
+                    // 提取游戏原价
+                    let originalPrice = try game.select("div.discount_original_price").text()
+                    // 提取游戏折扣价
+                    let discountPrice = try game.select("div.discount_final_price").text()
+                    // 提取游戏折扣比例
+                    let discountPercent = try game.select("div.discount_pct").text()
+                    // 获取img对象
+                    let imgTag: Element = try! game.select("img").first()!
+                    // 提取游戏图片
+                    let pic = try imgTag.attr("src")
+                    let game = Game(title: title, originalPrice: originalPrice, discountPrice: discountPrice , discountPercent: discountPercent,picurl: pic)
+                    gamesArray.append(game)
+                }
+                DispatchQueue.main.async {
+                    self.games = gamesArray
+                }
+            } catch {
+                print("解析HTML失败：\(error.localizedDescription)")
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+}
+
+#Preview {
+    ContentView()
+}
